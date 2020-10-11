b566df681f70b3e69099a7e824fa8a6486c193bf
prescottbreeden <prescott.breeden@gmail.com>
2020-10-11 15:39:58 -0700

    updated to 1.0.2

--- src/utilities.ts
+++ src/utilities.ts
@@ -5,22 +5,6 @@ export const compose = (...fns: Function[]) => (...args: any[]) =>
   fns.reduceRight((res, fn) => [fn.call(null, ...res)], args)[0];
 
 /**
- *  Evaluate any two values for deep equality
- *  @param a any value
- *  @param b any value
- *  @return boolean
- */
-export const deepEqual = (a: unknown, b: unknown) => {
-  return JSON.stringify(a) === JSON.stringify(b);
-};
-
-export const upsertItem = <T>(items: T[], data: Partial<T>, index: number) => {
-  return items.map((item: T, itemIndex: number) => {
-    return itemIndex === index ? { ...item, ...data } : { ...item };
-  });
-};
-
-/**
  *  curry :: ((a, b, ...) -> c) -> a -> b -> ... -> c
  */
 export function curry(fn: Function) {
@@ -34,16 +18,6 @@ export function curry(fn: Function) {
     return fn.call(null, ...args);
   };
 }
-
-/**
- *  trim :: String -> String
- */
-export const trim = (s: string) => s.trim();
-
-/**
- *  head :: [a] -> a
- */
-export const head = (xs: any[]) => xs[0];
 
 /**
  *  map :: (a -> b) -> [a] -> [b]
@@ -66,7 +40,3 @@ const reduceTruthy = (prev: any, current: any) => {
 
 export const all = reduce(reduceTruthy, true);
 
-export const validEmail = (email: string) => {
-  const validEmailRegex = /^(([^<>()\\[\]\\.,;:\s@"]+(\.[^<>()\\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return validEmailRegex.test(String(email).toLowerCase());
-};
--- src/validations.ts
+++ src/validations.ts
@@ -1,6 +1,54 @@
 import { all, compose, map, prop } from './utilities';
-import { CustomValidation, ValidationSchema, ValidationState } from './index.d';
 
+interface ValidationFunction<S> {
+  (val: any, state: S): boolean;
+}
+
+interface ValidationProps<S> {
+  errorMessage: string;
+  validation: ValidationFunction<S>;
+}
+
+export interface ValidationSchema<S> {
+  [key: string]: ValidationProps<S>[];
+}
+
+export interface ValidationState {
+  [key: string]: {
+    isValid: boolean;
+    error: string;
+  };
+}
+
+export interface ValidationObject<S> {
+  getError: (property: keyof S) => string;
+  getFieldValid: (property: keyof S) => boolean;
+  isValid: boolean;
+  validate: (
+    property: keyof S,
+    value: unknown,
+    state: S
+  ) => boolean | undefined;
+  validateAll: (state: S) => boolean;
+  validateIfTrue: (
+    property: keyof S,
+    value: unknown,
+    state: S
+  ) => boolean | undefined;
+  validateOnBlur: (state: S) => (event: any) => unknown;
+  validateOnChange: (
+    onChange: (event: any) => unknown,
+    state: S
+  ) => (event: any) => unknown;
+  validationErrors: string[];
+  validationState: ValidationState;
+}
+
+export interface CustomValidation {
+  key: string;
+  value: any;
+  state?: any;
+}
 
 export class Validation<S> {
   private _validationSchema: ValidationSchema<S>;
