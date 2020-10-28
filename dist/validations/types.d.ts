0b8ea02b3cebec449af9bdcd9d5bad21bb55f295
prescottbreeden <prescott.breeden@gmail.com>
2020-10-28 12:07:55 -0700

    updated validateAll back to strings

--- dist/validations/types.d.ts
+++ dist/validations/types.d.ts
@@ -20,7 +20,7 @@ export interface ValidationObject<S> {
     isValid: boolean;
     resetValidationState: () => void;
     validate: (property: keyof S, value: unknown, state?: S) => boolean | undefined;
-    validateAll: (state: S, keys?: [keyof S]) => boolean;
+    validateAll: (state: S, keys?: string[]) => boolean;
     validateCustom: (vals: CustomValidation[]) => boolean;
     validateIfTrue: (property: keyof S, value: unknown, state?: S) => boolean | undefined;
     validateOnBlur: (state: S) => (event: any) => unknown;
